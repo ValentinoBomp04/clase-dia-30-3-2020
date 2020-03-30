2a2efcc87af22192af48dd753c2c768d7555312d
vAlentinO116912231 <61870799+vAlentinO116912231@users.noreply.github.com>
2020-03-30 10:48:16 -0300

    Add files via upload

---
+++ ejercicio10.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+import sys
+
+num = int(input("Introduce un numero entero: "))
+if num/num==1 and num/1==num:
+	print (int(num), "es un numero primo")
+else: 
+	print "No es un numero primo"
+	
